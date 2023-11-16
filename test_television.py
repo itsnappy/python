d35f148ae73e6f99804dbcca717a8c3cb7c5fa57
Fawaze Ouro-Koura <104114593+itsnappy@users.noreply.github.com>
2023-11-16 17:19:43 -0600

    Update test_television.py
    Uses pytest instead of unittest

--- test_television.py
+++ test_television.py
@@ -1,43 +1,39 @@
-import unittest
+import pytest
 from television import Television
 
-class TestTelevision(unittest.TestCase):
+@pytest.fixture
+def tv_instance():
+    return Television()
 
-    def setUp(self):
-        self.tv_instance = Television()
+def test_init(tv_instance):
+    assert tv_instance._status == False
+    assert tv_instance._muted == False
+    assert tv_instance._volume == Television.MIN_VOLUME
+    assert tv_instance._channel == Television.MIN_CHANNEL
 
-    def test_init(self):
-        self.assertEqual(self.tv_instance._status, False)
-        self.assertEqual(self.tv_instance._muted, False)
-        self.assertEqual(self.tv_instance._volume, Television.MIN_VOLUME)
-        self.assertEqual(self.tv_instance._channel, Television.MIN_CHANNEL)
+def test_power(tv_instance):
+    tv_instance.power()
+    assert tv_instance._status == True
 
-    def test_power(self):
-        self.tv_instance.power()
-        self.assertEqual(self.tv_instance._status, True)
+def test_mute(tv_instance):
+    tv_instance.mute()
+    assert tv_instance._muted == True
 
-    def test_mute(self):
-        self.tv_instance.mute()
-        self.assertEqual(self.tv_instance._muted, True)
+def test_channel_up(tv_instance):
+    tv_instance.channel_up()
+    assert tv_instance._channel == Television.MIN_CHANNEL + 1
 
-    def test_channel_up(self):
-        self.tv_instance.channel_up()
-        self.assertEqual(self.tv_instance._channel, Television.MIN_CHANNEL + 1)
+def test_channel_down(tv_instance):
+    tv_instance.channel_down()
+    assert tv_instance._channel == Television.MAX_CHANNEL
 
-    def test_channel_down(self):
-        self.tv_instance.channel_down()
-        self.assertEqual(self.tv_instance._channel, Television.MAX_CHANNEL)
+def test_volume_up(tv_instance):
+    tv_instance.volume_up()
+    assert tv_instance._volume == Television.MIN_VOLUME + 1
 
-    def test_volume_up(self):
-        self.tv_instance.volume_up()
-        self.assertEqual(self.tv_instance._volume, Television.MIN_VOLUME + 1)
+def test_volume_down(tv_instance):
+    tv_instance.volume_down()
+    assert tv_instance._volume == Television.MIN_VOLUME
 
-    def test_volume_down(self):
-        self.tv_instance.volume_down()
-        self.assertEqual(self.tv_instance._volume, Television.MIN_VOLUME)
-
-    def test_str(self):
-        self.assertEqual(str(self.tv_instance), "Power = [False], Channel = [0], Volume = [0]")
-
-if __name__ == '__main__':
-    unittest.main()
+def test_str(tv_instance):
+    assert str(tv_instance) == "Power = [False], Channel = [0], Volume = [0]"
